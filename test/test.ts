f3f9f7f1dc083e24e6770714e3168b15e3010465
ravinthiranpartheepan1407 <ravinthiranpartheepan1407@gmail.com>
2023-12-27 01:34:02 +0200

    Mode + Word Frequency

--- test/test.ts
+++ test/test.ts
@@ -1,7 +1,12 @@
 import { mean } from "../src/mean";
 import { median } from "../src/median";
+import { mode } from "../src/mode";
+import { wordfreq } from "../src/wordfreq";
 
 const numbers: Array<number> = [10,20,30,40,60,50];
+var chars: Array<string> = ["Hello", "Welcome", "Hello", "Hi", "hello"]
 
 console.log(mean(numbers));
 console.log(median(numbers))
+console.log(mode(numbers))
+console.log(wordfreq(chars))
