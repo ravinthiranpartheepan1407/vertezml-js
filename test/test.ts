b5d4c288e62d7f408b4c7f5cb05a56bbf7676fa8
ravinthiranpartheepan1407 <ravinthiranpartheepan1407@gmail.com>
2023-12-27 00:51:38 +0200

    Median

--- test/test.ts
+++ test/test.ts
@@ -1,5 +1,7 @@
 import { mean } from "../src/mean";
+import { median } from "../src/median";
 
-const numbers: Array<number> = [10,20,30,40,50];
+const numbers: Array<number> = [10,20,30,40,60,50];
 
 console.log(mean(numbers));
+console.log(median(numbers))
